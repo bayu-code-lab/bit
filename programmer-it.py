023c2c371d0baecf07fe9a2a3498137e6a574a12
Bayu Satria Gemilang <59576135+bayu-code-lab@users.noreply.github.com>
2020-09-14 11:38:39 +0700

    Update programmer-it.py

--- programmer-it.py
+++ programmer-it.py
@@ -43,3 +43,30 @@ if __name__ == "__main__":
 
 	else : 
 		print("< no way >")
+		
+#3
+# python_3
+
+def wordBreak(dict, str, out=''):
+
+    if not str:
+        print(out)
+        return
+
+    for i in range(1, len(str) + 1):
+        prefix = str[:i]
+        if prefix in dict:
+            wordBreak(dict, str[i:], out + " " + prefix)
+
+
+if __name__ == '__main__':
+
+    # List dictionary
+    dict = [
+        "pro", "gram", "merit", "program", "it", "programmer"
+    ]
+
+    # input String
+    n = input("Your String: ")
+
+    wordBreak(dict, n)
