21eb50f0acf0db973223407224208382dbed210f
Bayu Satria Gemilang <59576135+bayu-code-lab@users.noreply.github.com>
2020-09-14 10:48:18 +0700

    Update programmer-it.py

--- programmer-it.py
+++ programmer-it.py
@@ -1,3 +1,4 @@
+#1
 def check(n):
     array =  ['pro', 'gram', 'merit', 'program', 'it', 'programmer']
     result = ''
@@ -14,3 +15,31 @@ def check(n):
 
 
 check('programmerit') #input value here
+
+#2
+def find_prefix(dictionary, word, temp = []):
+	for x in dictionary:
+		if word.startswith(x) :
+			temp.append(x)
+			word = word.split(x)[1]
+			return find_prefix(dictionary, word, temp)
+
+	return temp if not word else False
+
+if __name__ == "__main__":
+	dictionary = ['pro', 'gram', 'merit', 'program', 'it', 'programmer']
+	word = input("Enter your value: ") 
+
+	if word.startswith(tuple(dictionary)) :
+		for x in dictionary :
+			if word.startswith(x) :
+				if word == x :
+					print(x)
+				else : 
+					res = find_prefix(dictionary, word.split(x)[1],[x])
+					if res : 
+						print(', '.join(res))
+
+
+	else : 
+		print("< no way >")
